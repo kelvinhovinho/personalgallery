1896de6570c8aaa085133b7245c88508f1358a56
kelvin <kelvinadamba@hotmail.com>
2021-10-11 12:20:25 +0300

    added cloudinary credential settings in settings.py

--- gallary/settings.py
+++ gallary/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'gallary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
