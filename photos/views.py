e3a37f3ec8c00bb40de35c805349acdaa13292c5
kelvin <kelvinadamba@hotmail.com>
2021-10-11 13:18:58 +0300

    imported photos in views

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import photos
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    # importing photos and saving it in database
+    photo = photos.objects.all()
+
+    #adding context
+    ctx = {'photo': photo}
+    return render(request, 'index.html', ctx)
