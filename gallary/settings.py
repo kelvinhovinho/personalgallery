a9789b6941bc0db90e06daccf0b64dbc9e12230a
kelvin <kelvinadamba@hotmail.com>
2021-10-11 12:29:43 +0300

    added models in photos app

--- gallary/settings.py
+++ gallary/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'gallary.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'photos',
     }
 }
 
--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class photos(model.Models):
+    title = models.CharField(max_length=100)
+    image = CloudinaryField('image')
