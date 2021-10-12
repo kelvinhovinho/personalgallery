dd666ee503075252cc705969bd79f2a55a8b908d
kelvin <kelvinadamba@hotmail.com>
2021-10-12 11:55:02 +0300

    registered location and category in admin.py

--- photos/admin.py
+++ photos/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.db.models.fields.files import ImageFileDescriptor
-from .models import photos
+from .models import category, location, photos
 
 # Register your models here.
 admin.site.register(photos)
+admin.site.register(location)
+admin.site.register(category)
 
