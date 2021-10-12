2018dd4f8c98e0b70dca9f6e458da1e96066ad9b
kelvin <kelvinadamba@hotmail.com>
2021-10-12 11:46:19 +0300

    added location in models.py

--- photos/admin.py
+++ photos/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.db.models.fields.files import ImageFileDescriptor
 from .models import photos
 
 # Register your models here.
 admin.site.register(photos)
+
--- photos/models.py
+++ photos/models.py
@@ -2,19 +2,23 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 import datetime as dt
 
+from django.db.models.fields import CharField
+
 # Create your models here.
 class photos(models.Model):
     #title field
     title = models.CharField(max_length=100)
     #image field
     image = CloudinaryField('image')
-    # posted_date = models.DateTimeField(auto_now_add=True)
-    # description = models.CharField(max_length=200)
-    # location = models.ForeignKey('location', on_delete=models.CASCADE)
-    # category = models.ForeignKey('category', on_delete=models.CASCADE)
+    Description = models.CharField(max_length=200)
+    location = models.ForeignKey(location,on_delete=True)
 
-    # def saveImage(self):
-    #     self.save()
-    
-    # def deleteaimage(self):
-    #     self.delete()
+
+class location(models.Model):
+    locationName = CharField(max_length=30)
+
+    def saveLocation(self):
+        self.saveLocation()
+
+    def deleteLocation(self):
+        self.deleteLocation()
