c2f05491cdc2e104b3601685f0a15b7a98779b11
kelvin <kelvinadamba@hotmail.com>
2021-10-13 14:03:56 +0300

    imported settings to url.py

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,16 @@
-from django.contrib import admin
 from django.urls import path
+from django.urls.conf import re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('',views.index, name = 'index'),
+    re_path('location/(?p<location>\w+)',views.location,name = 'location'),
+    path('search/', views.search_results, name='search_results')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns = static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
